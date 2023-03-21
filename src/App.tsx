d6e5eaa761463a60c358c5c8cd3c4fe593075e69
Yuri Ramos <yuri.pramos@gmail.com>
2023-03-21 14:02:24 -0300

    set up mocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-
+import type { User } from './types';
+import UserDisplay from './components/UserDisplay';
 
 function App() {
+  const users: User[] = [
+    {
+      name: 'John Doe',
+      messages: [
+        {
+          body: 'hey you',
+        },
+        {
+          body: 'whats up',
+        },
+      ],
+    },
+  ];
 
   return (
     <div className="bg-zinc-500 flex-col-l h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      <h2 className="text-4xl text-yellow-500 ">
-        Hello Bitches
-      </h2>
+      {users.map((user, index) => (
+        <UserDisplay user={user} key={index} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
