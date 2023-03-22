5b027c71553e557c8e9e6d91271cd1c237b32013
Yuri Ramos <yuri.pramos@gmail.com>
2023-03-22 13:13:38 -0300

    setup graphQL query client and codegen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import type { User } from './types';
 import UserDisplay from './components/UserDisplay';
 
+import { useQuery } from 'urql';
+import { GetUsersDocument } from './graphql/generated';
+
 function App() {
   const users: User[] = [
     {
@@ -16,9 +19,15 @@ function App() {
     },
   ];
 
+  const [results] = useQuery({
+    query: GetUsersDocument,
+  });
+
+  console.log({ results });
+
   return (
     <div className="bg-zinc-500 flex-col-l h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
-      {users.map((user, index) => (
+      {results.data?.users.map((user, index) => (
         <UserDisplay user={user} key={index} />
       ))}
     </div>
