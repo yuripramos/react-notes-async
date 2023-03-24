8d2c7c5af0d36275e22e7de9b9d7bfcc27c54d7d
Yuri Ramos <yuri.pramos@gmail.com>
2023-03-24 16:50:56 -0300

    Modify schema.prisma

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -2,19 +2,6 @@ import { createServer } from 'node:http';
 import { createYoga } from 'graphql-yoga';
 import { schema } from './schema';
 
-// export const schema = createSchema({
-//   typeDefs: /* GraphQL */ `
-//     type Query {
-//       hello: String
-//     }
-//   `,
-//   resolvers: {
-//     Query: {
-//       hello: () => 'world',
-//     },
-//   },
-// });
-
 const port = Number(process.env.API_PORT) || 4000;
 const yoga = createYoga({ schema });
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import type { User } from './types';
 import UserDisplay from './components/UserDisplay';
 
 import { useQuery } from 'urql';
 import { GetUsersDocument } from './graphql/generated';
 
 function App() {
-  const users: User[] = [
-    {
-      name: 'John Doe',
-      messages: [
-        {
-          body: 'hey you',
-        },
-        {
-          body: 'whats up',
-        },
-      ],
-    },
-  ];
-
   const [results] = useQuery({
     query: GetUsersDocument,
   });
-
-  console.log({ results });
 
   return (
     <div className="bg-zinc-500 flex-col-l h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
