8abd522db1c77f3639ec970bc0bd97cc101b1791
Yuri Ramos <yuri.pramos@gmail.com>
2023-03-24 22:11:46 -0300

    Refactor seed

--- graphql-server/prisma/seed.ts
+++ graphql-server/prisma/seed.ts
@@ -6,48 +6,75 @@ async function main() {
   // Delete all `User` and `Message` records
   await prisma.message.deleteMany({});
   await prisma.user.deleteMany({});
+  await prisma.countries.deleteMany({});
+  await prisma.post.deleteMany({});
 
   // (Re-)Create dummy `User` and `Message` records
+  await prisma.user.createMany({
+    data: [
+      {
+        name: 'Kyle',
+        email: 'oneemail@yahoo.com',
+      },
+      {
+        name: 'Mao',
+        email: 'MaoTseTung@yahoo.com',
+      },
+      {
+        name: 'Jeremy',
+        email: 'jenjen@gmail.com',
+      },
+    ],
+  });
+
   await prisma.user.create({
     data: {
-      name: 'Jack',
+      name: 'Mastrucia',
+      email: 'nonewmail@gmail.com',
       messages: {
         create: [
           {
-            body: 'A Note for Jack',
+            body: 'message number one',
           },
           {
-            body: 'Another note for Jack',
+            body: 'message number two',
+          },
+          {
+            body: 'message number three',
+          },
+        ],
+      },
+      countries: {
+        create: [
+          {
+            list: 'Argentina, Brazil, Japan',
           },
         ],
       },
     },
   });
+
   await prisma.user.create({
     data: {
-      name: 'Ryan',
+      name: 'Jeremy',
+      email: 'Jeremy@gmail.com',
       messages: {
         create: [
           {
-            body: 'A Note for Ryan',
+            body: 'new new message number one',
           },
           {
-            body: 'Another note for Ryan',
+            body: 'new new message number two',
+          },
+          {
+            body: 'new new message number three',
           },
         ],
       },
-    },
-  });
-  await prisma.user.create({
-    data: {
-      name: 'Adam',
-      messages: {
+      countries: {
         create: [
           {
-            body: 'A Note for Adam',
-          },
-          {
-            body: 'Another note for Adam',
+            list: 'Australia, Singapore, Israel',
           },
         ],
       },
